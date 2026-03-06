46c472b3ec3708d4c1adadaeb719228b63ed88f5
Fernando Gasperi <fgasperijabalera@gmail.com>
2026-03-06 13:20:55 -0300

    refactor(pdfgate-api-error): add structured info to PdfGateApiError
    - truncate the error responses to prevent huge payloads
    - add struture fields to the error so the user doesn't need
    to parse the message

--- src/types/classes.ts
+++ src/types/classes.ts
@@ -1,6 +1,19 @@
+export type PdfGateApiErrorOptions = {
+  statusCode?: number;
+  responseBody?: string;
+  cause?: unknown;
+};
+
 export class PdfGateApiError extends Error {
-  constructor(message: string) {
+  statusCode?: number;
+  responseBody?: string;
+  cause?: unknown;
+
+  constructor(message: string, options: PdfGateApiErrorOptions = {}) {
     super(message);
     this.name = 'PdfGateApiError';
+    this.statusCode = options.statusCode;
+    this.responseBody = options.responseBody;
+    this.cause = options.cause;
   }
 }
